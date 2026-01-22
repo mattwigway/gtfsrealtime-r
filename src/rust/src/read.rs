3c0cdb5bfde65da69e71249f82a9e3602fb4074f
Matt Bhagat-Conway <matt@indicatrix.org>
2026-01-22 09:00:56 -0500

    transparently read gzipped and bzipped protobuf

--- src/rust/src/read.rs
+++ src/rust/src/read.rs
@@ -1,14 +1,35 @@
 use std::{fs::File, io::Read};
 
 use bytes::BytesMut;
+use bzip2::read::BzDecoder;
+use flate2::read::GzDecoder;
 use prost::Message;
 
 use crate::transit_realtime::FeedMessage;
+
+const BZIP2_MAGIC_BYTES: [u8; 3] = [b'B', b'Z', b'h'];
+const GZIP_MAGIC_BYTES: [u8; 3] = [0x1f, 0x8b, 0x08];
 
 pub fn read_feed(path: String) -> Result<FeedMessage, Box<dyn std::error::Error>> {
     let mut file = File::open(path)?;
     let mut buf = Vec::new();
     file.read_to_end(&mut buf)?;
+
+    let first_three = &buf[..3];
+
+    // transparently handle Bzipped or Gzipped data
+    if first_three == BZIP2_MAGIC_BYTES {
+        let mut buf2 = Vec::new();
+        let mut decompressor = BzDecoder::new(&buf[..]);
+        decompressor.read_to_end(&mut buf2)?;
+        buf = buf2;
+    } else if first_three == GZIP_MAGIC_BYTES {
+        let mut buf2 = Vec::new();
+        let mut decompressor = GzDecoder::new(&buf[..]);
+        decompressor.read_to_end(&mut buf2)?;
+        buf = buf2;
+    }
+
     let byt = BytesMut::from(buf.as_slice());
     let msg = FeedMessage::decode(byt)?;
     return Ok(msg);
