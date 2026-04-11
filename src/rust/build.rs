75b015e482ecf95cc8ac0e397e44e7d7135b836d
Matt Bhagat-Conway <matt@indicatrix.org>
2026-04-10 23:24:08 -0400

    print sitrep and create generated dir as needed

--- src/rust/build.rs
+++ src/rust/build.rs
@@ -1,13 +1,48 @@
+use std::env;
+use std::fs::create_dir;
 use std::io::Result;
 use std::path::Path;
+use std::process::Command;
 
 use prost_build::Config;
+
+fn print_path_exists(pathstr: &str) -> () {
+    let path = Path::new(pathstr);
+    if path.exists() {
+        eprintln!("{pathstr}: exists");
+    } else {
+        eprintln!("{pathstr}: does not exist");
+    }
+}
+
 fn main() -> Result<()> {
+    eprintln!("Running build script. Current situation:");
+    print_path_exists("src");
+    print_path_exists("src/generated");
+    print_path_exists("src/gtfs-realtime.proto");
+
+    if env::consts::OS == "linux" || env::consts::OS == "macos" {
+        match Command::new("which").arg("protoc").output() {
+            Ok(res) => {
+                let path = String::from_utf8(res.stdout).unwrap();
+                eprintln!("protoc found at {path}");
+            }
+            Err(_) => println!("protoc not found"),
+        }
+    } else {
+        println!("Not checking for protoc location on Windows");
+    }
+
     // generate generated rust code from protobuf, if needed
     // The reason we don't do this always is because protoc is unlikely to be
     // available on CRAN build machines. So transit_realtime.rs is built on
     // Github Actions and then rolled into the built package.
-    if !Path::exists(Path::new("src/generated/transit_realtime.rs")) {
+    if !Path::new("src/generated/transit_realtime.rs").exists() {
+        // for some reason, during pkgdown builds, src/generated is no longer present
+        if !Path::new("src/generated").exists() {
+            create_dir(Path::new("src/generated"))?;
+        }
+
         let deriv = "#[derive(strum_macros::VariantArray, strum_macros::IntoStaticStr, gtfsrt_macros::AsStrName)]";
         Config::new()
             // I can't figure out a way to tell prost to just apply this to all enums without also applying to structs
