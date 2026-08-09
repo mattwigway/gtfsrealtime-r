6ad6d7332b3b70d8edffc47f6c6d05499e491ff2
Matt Bhagat-Conway <matt@indicatrix.org>
2026-08-08 20:45:39 -0400

    clean up docs

--- R/updates.R
+++ R/updates.R
@@ -29,8 +29,14 @@
 #'      GTFS-realtime file, but sometimes are not. If there are non-unique IDs in the feed, they will
 #'      be made unique when data are loaded by appending `_duplicated_1`, `_duplicated_2`, and so on
 #'      and a warning will be issued, which guarantees that all rows from a single file have unique IDs.
+#'
 #'      When working with archived data, there will quite likely be duplicated IDs between files archived
-#'      at different times (path: `id` property of `FeedEntity` containing this `TripUpdate`).
+#'      at different times; in this case `file_index` should be used in combination with `id` to uniquely identify
+#'      vehicle positions. The GTFS-realtime specification makes no guarantees about the stability of
+#'      entity IDs between feeds, so these should not be used to join data from multiple GTFS-realtime
+#'      feeds (e.g. from different times of day).
+#'
+#'      (path: `id` property of `FeedEntity` containing this `TripUpdate`).
 #' - `trip_id`: The `trip_id` from the static GTFS feed that this selector refers to. For non frequency-based trips
 #'      (trips not defined in GTFS `frequencies.txt`), this field is enough to uniquely identify the trip. For
 #'      frequency-based trips defined in GTFS `frequencies.txt`, `trip_id`, `start_time`, and `start_date` are all
